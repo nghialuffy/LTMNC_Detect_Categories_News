f3e6da1e68a121966ea13e1e9df2ef3d9dd36d51
nghialuffy <nghialuffy@github.com>
2021-03-12 13:59:13 +0700

    Add CORS

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,12 @@ from flask_restful import Resource, Api
 from datetime import datetime
 from Controller.homepage import HomePage
 from Controller.detect_category import DetectCategory
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
-
+app.config["CORS_HEADERS"] = "Content-Type"
+cors = CORS(app)
 
 api.add_resource(HomePage, '/') # Home page 
 api.add_resource(DetectCategory, '/api/v1.0/detect_category')
